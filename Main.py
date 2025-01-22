df807023d4eae53272f74980205fd87a82491ecf
croissantaye <asa81099@gmail.com>
2025-01-21 23:38:44 -0800

    Add tail movement and adjustable snake length

--- Main.py
+++ Main.py
@@ -1,33 +1,18 @@
 # Example file showing a basic pygame "game loop"
 import pygame
 import math
-import random
 import numpy
 import Controls 
 import SnakePiece
 
 # pygame setup
 pygame.init()
-grid = pygame.Vector2(40, 30)
-squareSize = 30
-screen = pygame.display.set_mode((grid.x * squareSize, grid.y * squareSize))
-clock = pygame.time.Clock()
-frameRate = 60
-running = True
-dt = 0
-speed = 0.5 
-frameCounter = 0
-movementVector = pygame.Vector2(0,0)
-input = pygame.Vector2(0, 0)
 
-screenCenter = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
-headPos = screenCenter
-snakeSquares = [SnakePiece.SnakePiece(pygame.Rect(0, 0, squareSize, squareSize)), 
-# SnakePiece.SnakePiece(pygame.Rect(0, 0, squareSize, squareSize)),
-# SnakePiece.SnakePiece(pygame.Rect(0, 0, squareSize, squareSize)),
-# SnakePiece.SnakePiece(pygame.Rect(0, 0, squareSize, squareSize)),
-# SnakePiece.SnakePiece(pygame.Rect(0, 0, squareSize, squareSize)),
-]
+def createSnakePieces(startPos: pygame.Vector2, numPieces: int) -> list:
+    pieces = list()
+    for i in range(numPieces):
+        pieces.append(SnakePiece.SnakePiece(pygame.Vector2(startPos.x, startPos.y - (squareSize * i)), pygame.Vector2(), squareSize))
+    return pieces
 
 def drawScreenGrid(color):
         screen.fill(pygame.Color(255, 255, 255, 255))
@@ -40,6 +25,22 @@ def drawScreenGrid(color):
                 if(y % 2 == yOffset):
                     tile = pygame.Rect(squareSize * x, squareSize * y, squareSize, squareSize)
                     pygame.draw.rect(screen, color, tile)
+
+grid = pygame.Vector2(40, 30)
+squareSize = 20
+screen = pygame.display.set_mode((grid.x * squareSize, grid.y * squareSize))
+clock = pygame.time.Clock()
+frameRate = 60
+running = True
+dt = 0
+speed = 1
+movementVector = pygame.Vector2(0, 1)
+input = pygame.Vector2(0, 1)
+
+screenCenter = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+headPos = screenCenter
+snakeSquares = createSnakePieces(screenCenter, 5)
+snakeSquares[0].dir = pygame.Vector2(0, speed)
 
 while running:
     # poll for events
@@ -54,21 +55,11 @@ while running:
         numpy.clip(headPos.y + (movementVector.y * speed), 0, screen.get_height() - squareSize)
     )
 
-    roundedHeadPos = pygame.Vector2(math.floor(headPos.x), math.floor(headPos.y))
     remainder = 0
     color = 'grey'
-    input = Controls.getMovementVector(input, speed, frameRate)
-    print('update input ' + str(frameCounter))
-    print(input)
-    print('movement')
-    print(movementVector)
-    print(input.dot(movementVector))
-    print(roundedHeadPos.x % squareSize)
-    print(roundedHeadPos.y % squareSize)
-    print(headPos)
+    input = Controls.getMovementVector(input)
 
     if(headPos.x % squareSize <= remainder and headPos.y % squareSize <= remainder):
-        # color = 'orange'
         if(input.dot(movementVector) >= 0):
             movementVector = input
 
@@ -85,35 +76,40 @@ while running:
                     
     # RENDER YOUR GAME HERE
     numSnakeSquares = len(snakeSquares)
-    previousSnakeSquarePos = headPos 
+    prevPos = headPos
+    headColor = "red"
+    tailColor = "black"
+    snakeColor = headColor 
     for i in range(numSnakeSquares):
         if(i == 0):
-            snakeSquares[i].setRectPos(headPos)
-            snakeSquares[i].setPos(headPos)
-            pygame.draw.rect(screen, "red", snakeSquares[i])
-        # else:
-            # previousSnakeSquarePos = snakeSquares[i -1].previousPos
-            # normalMovementVector = pygame.Vector2(0, 0)
-            # if movementVector.magnitude() != 0:
-            #     normalMovementVector = movementVector.normalize()
-            # offset = pygame.Vector2(normalMovementVector.x * squareSize, normalMovementVector.y * squareSize)
-            # previousSnakeSquarePos = pygame.Vector2(previousSnakeSquarePos.x - offset.x, previousSnakeSquarePos.y - offset.y)
-            
-            # snakeSquares[i].setRectPos(previousSnakeSquarePos)
-            # snakeSquares[i].setPos(previousSnakeSquarePos)
-            # pygame.draw.rect(screen, pygame.Color(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)), snakeSquares[i])
+            snakeSquares[i].updatePos(headPos, movementVector)
+        else:
+            if(snakeColor != tailColor):
+                snakeColor = tailColor
+            square = snakeSquares[i]
+            dir = square.dir
+            if(square.pos.x % squareSize == 0 and square.pos.y % squareSize == 0):
+                if(abs(prevPos.x - square.pos.x) > abs(prevPos.y - square.pos.y)):
+                    dir = pygame.Vector2(math.copysign(1, prevPos.x - square.pos.x), 0)
+                else:
+                    dir = pygame.Vector2(0, math.copysign(1, prevPos.y - square.pos.y))
+            square.updatePos(pygame.Vector2(square.pos.x + (dir.x * speed), square.pos.y + (dir.y * speed)), dir)
+        
+        pygame.draw.rect(screen, snakeColor, snakeSquares[i].rect)
+        prevPos = snakeSquares[i].pos
 
     # if collision, end game
     if(isWallCollision):
         running = False
-    
+
     # flip() the display to put your work on screen
     pygame.display.flip()
 
     # limits FPS to 60
     # dt is delta time in seconds since last frame, used for framerate-
     # independent physics.
-    frameCounter += 1
     dt = clock.tick(frameRate) / 1000
+
+    print()
 
 pygame.quit()
--- SnakePiece.py
+++ SnakePiece.py
@@ -1,21 +1,30 @@
 import pygame
 
 class SnakePiece:
-    def __init__(self, rect):
-        self.pos = pygame.Vector2(0, 0)
-        self.rect = rect
-        self.previousPos = pygame.Vector2(0, 0)
+    def __init__(self, pos: pygame.Vector2, dir: pygame.Vector2, size: int):
+        self.rect = pygame.Rect(pos.x, pos.y, size, size) 
+        self.pos = pos
+        self.dir = dir
+        self.prevPos = pos
 
-    def setPos(self, pos):
-        self.previousPos = self.pos
+    def updatePos(self, pos: pygame.Vector2, dir: pygame.Vector2):
+        self.setPos(pos)
+        self.setRectPos(pos)
+        self.setDir(dir)
+
+    def setPos(self, pos: pygame.Vector2):
+        self.prevPos = self.pos
         self.pos = pos 
+
+    def setDir(self, dir: pygame.Vector2):
+        self.dir = pygame.Vector2(dir.x, dir.y)
         
-    # def pos():
-    #     return self.__pos
+    def setRectPos(self, pos: pygame.Vector2):
+        # self.rect.move(pos.x - self.prevPos.x, pos.y - self.prevPos.y)
+        self.rect.x = int(pos.x)
+        self.rect.y = int(pos.y)
 
-    # def rect():
-    #     return self.__rect
-
-    def setRectPos(self, pos):
-        self.rect.x = pos.x
-        self.rect.y = pos.y
+    def __str__(self) -> str:
+        temp = f"pos: {self.pos},\ndir: {self.dir}\n"
+        rectStr = str(self.rect)
+        return temp + rectStr
